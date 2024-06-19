e76ca9ce30941c78b4cc5f4531d2a0377e472255
Denis Jongmanee <denis.jongmanee@biopath.fr>
2024-06-19 20:15:57 +0200

    feat(commande): Ajout des routes CRUD de la table commande

---
+++ app/routers/commande.py
@@ -0,0 +1,93 @@
+from typing import List
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from app.connexion import get_db
+from app import actions, schemas, models
+
+router = APIRouter()
+
+@router.get("", response_model=List[schemas.Commande])
+async def get_commandes(database: Session = Depends(get_db)):
+    """
+        Retourne toutes les commandes
+    """
+    try:
+        db_commandes = actions.get_commandes(database)
+
+        return db_commandes
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
+
+@router.get("/{id_commande}", response_model=schemas.Commande)
+async def get_commande(id_commande: int, database: Session = Depends(get_db)):
+    """
+        Retourne la commande trouvé par son id
+    """
+    try:
+        db_commande = actions.get_commande(id_commande, database)
+
+        if db_commande is None:
+            raise HTTPException(status_code=404, detail="Commande not found")
+
+        return db_commande
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
+
+@router.post("", response_model=schemas.Commande, status_code=201)
+async def post_commande(commande: schemas.CommandeCreate, database: Session = Depends(get_db)):
+    """
+        Créer une nouvelle commande
+    """
+    try:
+        new_commande = models.Commande(
+            date_commande=commande.date_commande,
+            montant_total=commande.montant_total,
+            payee=commande.payee,
+            status_livraison=commande.status_livraison,
+            adresse_livraison=commande.adresse_livraison,
+            id_client=commande.id_client,
+        )
+        db_commande = actions.create_commande(new_commande, database)
+
+        return db_commande
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
+
+@router.delete("/{id_commande}")
+async def delete_commande(id_commande: int, database: Session = Depends(get_db)):
+    """
+        Supprime une commande
+    """
+    try:
+        db_commande = actions.get_commande(id_commande, database)
+        if db_commande is None:
+            raise HTTPException(status_code=404, detail="Commande not found")
+
+        actions.delete_commande(db_commande, database)
+
+        return {"deleted": id_commande}
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
+
+@router.patch("/{id_commande}", response_model=schemas.Commande)
+async def patch_commande(id_commande: int,
+    commande: schemas.CommandeUpdate, database: Session = Depends(get_db)):
+    """
+        Met à jour les données de la commande
+    """
+    try:
+        db_commande = actions.get_commande(id_commande, database)
+        if db_commande is None:
+            raise HTTPException(status_code=404, detail="Commande not found")
+
+        db_commande = actions.update_commande(db_commande, commande, database)
+
+        return db_commande
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
---
+++ app/test/test_commande.py
@@ -0,0 +1,289 @@
+from datetime import datetime
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from app import models, actions
+from app.models import Base
+from app.main import app
+
+client = TestClient(app)
+
+def memory_engine():
+    """
+        Créer un engine sqlalchemy qui utilise la mémoire
+    """
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    session_factory = sessionmaker(bind=engine)
+    database = scoped_session(session_factory)
+
+    return database
+
+def test_get_commandes(mocker):
+    """
+        Cas passant (retourne la liste de commande)
+    """
+    commandes = [{
+       "id_commande": 1,
+        "date_commande": "2023-10-10T10:10:00",
+        "montant_total": 23.12,
+        "payee": True,
+        "status_livraison": "expédiée",
+        "adresse_livraison": "9 rue de la Monnaie 59000 Lille",
+        "id_client": 1
+    }]
+    mocker.patch("sqlalchemy.orm.Session.query", return_value=commandes)
+
+    response = client.get("/commande")
+
+    assert response.status_code == 200
+    assert response.json() == commandes
+
+def test_get_commandes_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    mocker.patch("sqlalchemy.orm.Session.query", side_effect=Exception("Connection error"))
+
+    response = client.get("/commande")
+
+    assert response.status_code == 500
+
+def test_get_commande(mocker):
+    """
+        Cas passant (retourne une commande)
+    """
+    db_commande = {
+       "id_commande": 1,
+        "date_commande": "2023-10-10T10:10:00",
+        "montant_total": 23.12,
+        "payee": True,
+        "status_livraison": "expédiée",
+        "adresse_livraison": "9 rue de la Monnaie 59000 Lille",
+        "id_client": 1
+    }
+    mock_query = mocker.MagicMock()
+    mock_query.where.return_value.first.return_value = db_commande
+    mocker.patch("sqlalchemy.orm.Session.query", return_value=mock_query)
+
+    response = client.get("/commande/" + str(db_commande['id_commande']))
+
+    assert response.status_code == 200
+    assert response.json() == db_commande
+
+def test_get_commande_error_404(mocker):
+    """
+        Cas non passant (la commande n'a pas été trouvée)
+    """
+    db_commande = None
+    mock_query = mocker.MagicMock()
+    mock_query.where.return_value.first.return_value = db_commande
+    mocker.patch("sqlalchemy.orm.Session.query", return_value=mock_query)
+
+    response = client.get("/commande/1")
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Commande not found'}
+
+def test_get_commande_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    mocker.patch("sqlalchemy.orm.Session.query", side_effect=Exception("Connection error"))
+
+    response = client.get("/commande/1")
+
+    assert response.status_code == 500
+
+def test_post_commande(mocker):
+    """
+        Cas passant (retourne la commande avec un id)
+    """
+    new_commande = {
+        "date_commande": "2023-10-10T10:10:00",
+        "montant_total": 23.12,
+        "payee": True,
+        "status_livraison": "expédiée",
+        "adresse_livraison": "9 rue de la Monnaie 59000 Lille",
+        "id_client": 1
+    }
+    db_commande = models.Commande(
+        id_commande=1,
+        date_commande=new_commande['date_commande'],
+        montant_total=new_commande['montant_total'],
+        payee=new_commande['payee'],
+        status_livraison=new_commande['status_livraison'],
+        adresse_livraison=new_commande['adresse_livraison'],
+        id_client=new_commande['id_client']
+    )
+
+    mocker.patch("app.actions.create_commande", return_value=db_commande)
+
+    response = client.post("/commande", json=new_commande)
+
+    assert response.status_code == 201
+
+def test_action_create_commande():
+    """
+        Test unitaire de la function create commande
+    """
+    database = memory_engine()
+    new_commande = models.Commande(
+        date_commande=datetime.now(),
+        montant_total=23.12,
+        payee=True,
+        status_livraison="expédiée",
+        adresse_livraison="9 rue de la Monnaie 59000 Lille",
+        id_client=1
+    )
+
+    db_commande = actions.create_commande(new_commande, database)
+
+    assert isinstance(db_commande, models.Commande)
+    assert db_commande.id_commande is not None
+
+def test_post_commande_error_422():
+    """
+        Cas non passant (des informations de la commande sont manquants)
+    """
+    new_commande = {
+        "payee": True,
+        "status_livraison": "expédiée",
+        "adresse_livraison": "9 rue de la Monnaie 59000 Lille",
+        "id_client": 1
+    }
+
+    response = client.post("/commande", json=new_commande)
+
+    assert response.status_code == 422
+
+def test_post_commande_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    new_commande = {
+        "date_commande": "2023-10-10T10:10:00",
+        "montant_total": 23.12,
+        "payee": True,
+        "status_livraison": "expédiée",
+        "adresse_livraison": "9 rue de la Monnaie 59000 Lille",
+        "id_client": 1
+    }
+    mocker.patch("sqlalchemy.orm.Session.commit", side_effect=Exception("Connection error"))
+
+    response = client.post("/commande", json=new_commande)
+
+    assert response.status_code == 500
+
+def test_delete_commande(mocker):
+    """
+        Cas passant (retourne l'id de la commande supprimée)
+    """
+    db_commande = models.Commande(
+        id_commande=1,
+        date_commande=datetime.now(),
+        montant_total=23.12,
+        payee=True,
+        status_livraison="expédiée",
+        adresse_livraison="9 rue de la Monnaie 59000 Lille",
+        id_client=1
+    )
+    mocker.patch("app.actions.get_commande", return_value=db_commande)
+    mocker.patch("sqlalchemy.orm.Session.delete", return_value=None)
+    mocker.patch("sqlalchemy.orm.Session.commit", return_value=None)
+
+    response = client.delete("/commande/" + str(db_commande.id_commande))
+
+    assert response.status_code == 200
+    assert response.json() == {"deleted": db_commande.id_commande}
+
+def test_delete_commande_error_404(mocker):
+    """
+        Cas non passant (la commande n'est pas trouvée)
+    """
+    mocker.patch("app.actions.get_commande", return_value=None)
+
+    response = client.delete("/commande/1")
+
+    assert response.status_code == 404
+
+def test_delete_commande_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    db_commande = models.Commande(
+        id_commande=1,
+        date_commande=datetime.now(),
+        montant_total=23.12,
+        payee=True,
+        status_livraison="expédiée",
+        adresse_livraison="9 rue de la Monnaie 59000 Lille",
+        id_client=1
+    )
+    mocker.patch("app.actions.get_commande", return_value=db_commande)
+    mocker.patch("sqlalchemy.orm.Session.delete", side_effect=Exception("Connection error"))
+
+    response = client.delete("/commande/1")
+
+    assert response.status_code == 500
+
+def test_patch_commande(mocker):
+    """
+        Cas passant (retourne la commande mise à jour)
+    """
+    db_commande = models.Commande(
+        id_commande=1,
+        date_commande=datetime.now(),
+        montant_total=23.12,
+        payee=True,
+        status_livraison="en préparation",
+        adresse_livraison="9 rue de la Monnaie 59000 Lille",
+        id_client=1
+    )
+    commande_updated = {
+        "status_livraison": "expédiée",
+    }
+    mocker.patch("app.actions.get_commande", return_value=db_commande)
+    mocker.patch("sqlalchemy.orm.Session.commit", return_value=None)
+
+    response = client.patch("/commande/" + str(db_commande.id_commande), json=commande_updated)
+
+    assert response.status_code == 200
+    assert response.json()["status_livraison"] == commande_updated["status_livraison"]
+
+def test_patch_commande_error_404(mocker):
+    """
+         Cas non passant (la commande n'est pas trouvée)
+    """
+    commande_updated = {
+        "status_livraison": "expédiée",
+    }
+    mocker.patch("app.actions.get_commande", return_value=None)
+
+    response = client.patch("/commande/1", json=commande_updated)
+
+    assert response.status_code == 404
+
+def test_patch_commande_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    db_commande = models.Commande(
+        id_commande=1,
+        date_commande=datetime.now(),
+        montant_total=23.12,
+        payee=True,
+        status_livraison="en préparation",
+        adresse_livraison="9 rue de la Monnaie 59000 Lille",
+        id_client=1
+    )
+    commande_updated = {
+        "status_livraison": "expédiée",
+    }
+    mocker.patch("app.actions.get_commande", return_value=db_commande)
+    mocker.patch("sqlalchemy.orm.Session.commit", side_effect=Exception("Connection error"))
+
+    response = client.patch("/commande/" + str(db_commande.id_commande), json=commande_updated)
+
+    assert response.status_code == 500
